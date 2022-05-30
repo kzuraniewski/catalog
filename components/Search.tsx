4525bcbb267b5231b99d169aa867cccc2b71a22c
nick-here <zuraniewski.karol@gmail.com>
2022-05-30 12:25:28 +0200

    input overflow fix

--- components/Search.tsx
+++ components/Search.tsx
@@ -59,9 +59,6 @@ const Search = ({
 						paddingLeft: `calc(1em + ${theme.spacing(4)})`,
 						transition: theme.transitions.create('width'),
 						width: '100%',
-						[theme.breakpoints.up('md')]: {
-							width: '20ch',
-						},
 					},
 				}}
 				{...InputBaseProps}
