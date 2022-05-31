0909b90d6b50c31b100385d1721ec6fc1899d440
nick-here <zuraniewski.karol@gmail.com>
2022-05-31 14:36:56 +0200

    logo as a link

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import Image, { ImageProps } from 'next/image';
+import Link from 'next/link';
 
 export interface LogoProps extends ImageProps {}
 
 const Logo = ({ ...other }: LogoProps) => {
 	return (
-		<Image
-			layout="responsive"
-			width={1157}
-			height={716}
-			alt="Logo"
-			priority
-			{...other}
-			src="/logo.png"
-		></Image>
+		<Link href="/">
+			<a>
+				<Image
+					layout="responsive"
+					width={1157}
+					height={716}
+					alt="Logo"
+					priority
+					{...other}
+					src="/logo.png"
+				></Image>
+			</a>
+		</Link>
 	);
 };
 
