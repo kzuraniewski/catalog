30fab2b97a84a2e955f3d91a49fdf4604a1eb367
nick-here <zuraniewski.karol@gmail.com>
2022-05-31 10:43:51 +0200

    main website button

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { AppBar, Toolbar } from '@mui/material';
+import { AppBar, Box, Toolbar } from '@mui/material';
 import HeaderButton from './HeaderButton';
 import { drawerWidth } from './Sidenav';
+import { ArrowBack } from '@mui/icons-material';
 // import logo from '../assets/logo.svg';
+
+const mainWebsiteUrl = 'http://agrofrankopol.strona-projekt.pl';
 
 const Header = () => {
 	return (
@@ -11,6 +14,10 @@ const Header = () => {
 			sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
 		>
 			<Toolbar>
+				<HeaderButton href={mainWebsiteUrl} startIcon={<ArrowBack />}>
+					Strona główna
+				</HeaderButton>
+				<Box flexGrow={1} />
 				<HeaderButton>Login</HeaderButton>
 			</Toolbar>
 		</AppBar>
