acd099d5012c9948c1cf969894855fc91c77f232
nick-here <zuraniewski.karol@gmail.com>
2022-06-01 08:56:16 +0200

    document preview component

---
+++ components/DocumentCard.tsx
@@ -0,0 +1,45 @@
+import React, { useRef, useState } from 'react';
+import {
+	Button,
+	Card,
+	CardActionArea,
+	CardActions,
+	CardContent,
+	CardProps,
+	Divider,
+	Typography,
+} from '@mui/material';
+import { BookmarkAdd, BookmarkRemove } from '@mui/icons-material';
+
+export interface DocumentCardProps extends CardProps {}
+
+const DocumentCard = ({ ...other }: DocumentCardProps) => {
+	const [saved, setSaved] = useState(false);
+
+	return (
+		<Card sx={{ maxWidth: 300 }} {...other}>
+			<CardActionArea disableRipple>
+				<CardContent>
+					<Typography variant="h3" sx={{ mb: 2 }}>
+						Lorem ipsum
+					</Typography>
+					<Typography variant="body1">
+						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam placeat
+						exercitationem nisi consectetur.
+					</Typography>
+				</CardContent>
+			</CardActionArea>
+			<Divider />
+			<CardActions>
+				<Button
+					startIcon={saved ? <BookmarkRemove /> : <BookmarkAdd />}
+					onClick={() => setSaved(saved => !saved)}
+				>
+					{saved ? 'Usuń z zapisanych' : 'Zapisz'}
+				</Button>
+			</CardActions>
+		</Card>
+	);
+};
+
+export default DocumentCard;
