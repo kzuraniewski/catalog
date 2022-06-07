055f60cebe0c0c88990f90f73ca47863c7da2a95
nick-here <zuraniewski.karol@gmail.com>
2022-06-07 11:38:16 +0200

    document styling + pagination

--- components/documents/DocumentPreview.tsx
+++ components/documents/DocumentPreview.tsx
@@ -1,17 +1,52 @@
 import React, { useState } from 'react';
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack5';
 import type { DocumentProps } from 'react-pdf';
+import { Box, Paper, Pagination } from '@mui/material';
 
-export interface DocumentPreviewProps extends DocumentProps {}
+export interface DocumentPreviewProps extends Pick<DocumentProps, 'file'> {}
 
-const DocumentPreview = ({ file, ...other }: DocumentPreviewProps) => {
+const DocumentPreview = ({ file }: DocumentPreviewProps) => {
 	const [pageNumber, setPageNumber] = useState(1);
+	const [pageCount, setPageCount] = useState(0);
 
 	return (
-		<Document file={file} {...other}>
-			<Page pageNumber={pageNumber} />
-		</Document>
+		<Box position="relative">
+			<Document file={file} onLoadSuccess={({ numPages }) => setPageCount(numPages)}>
+				<Paper
+					elevation={3}
+					sx={{
+						mx: 'auto',
+						width: 'min-content',
+						minWidth: 300,
+						minHeight: 200,
+					}}
+				>
+					<Page pageNumber={pageNumber} />
+				</Paper>
+			</Document>
+
+			{pageCount >= 2 && (
+				<Box
+					sx={{
+						position: 'absolute',
+						bottom: 10,
+						left: '50%',
+						transform: 'translateX(-50%)',
+					}}
+				>
+					<Pagination
+						count={pageCount}
+						color="primary"
+						shape="rounded"
+						page={pageNumber}
+						onChange={(_e, page) => setPageNumber(page)}
+					/>
+				</Box>
+			)}
+		</Box>
 	);
 };
 
 export default DocumentPreview;
+
+//TODO: i18n
