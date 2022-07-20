f90a1a84f3eb3c7c870ccb524244f161cb285dd3
nick-here <zuraniewski.karol@gmail.com>
2022-07-20 17:39:34 +0200

    Annotation tooltip removed

--- src/components/documents/document-preview/Annotation.tsx
+++ src/components/documents/document-preview/Annotation.tsx
@@ -1,4 +1,4 @@
-import { Box, BoxProps, Tooltip, Typography } from '@mui/material';
+import { Box, BoxProps, Typography } from '@mui/material';
 import React from 'react';
 
 export interface AnnotationProps extends BoxProps {
@@ -9,38 +9,36 @@ export interface AnnotationProps extends BoxProps {
 const Annotation = ({ anchorName, highlighted = false, ...other }: AnnotationProps) => {
 	return (
 		<Box position="relative" {...other}>
-			<Tooltip title={`Odnośnik ${anchorName}`} placement="top" arrow>
-				<Box
-					className="renderer"
-					sx={theme => ({
-						position: 'absolute',
-						userSelect: 'none',
-						display: 'flex',
-						justifyContent: 'center',
-						alignItems: 'center',
+			<Box
+				className="renderer"
+				sx={theme => ({
+					position: 'absolute',
+					userSelect: 'none',
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
 
-						width: '1.2rem',
-						height: '1.2rem',
-						transform: 'translate(-0.2rem, -0.2rem)',
+					width: '1.2rem',
+					height: '1.2rem',
+					transform: 'translate(-0.2rem, -0.2rem)',
 
-						transition: 'color 0.1s',
-						color: 'secondary.contrastText',
-						borderRadius: theme.shape.borderRadius,
-						backgroundColor: 'secondary.main',
-						outline: theme =>
-							highlighted ? `2px solid ${theme.palette.primary.light}` : 'none',
+					transition: 'color 0.1s',
+					color: 'secondary.contrastText',
+					borderRadius: theme.shape.borderRadius,
+					backgroundColor: 'secondary.main',
+					outline: theme =>
+						highlighted ? `2px solid ${theme.palette.primary.light}` : 'none',
 
-						'&:hover': {
-							backgroundColor: 'secondary.light',
-							boxShadow: 1,
-						},
-					})}
-				>
-					<Typography variant="button" align="center">
-						{anchorName}
-					</Typography>
-				</Box>
-			</Tooltip>
+					'&:hover': {
+						backgroundColor: 'secondary.light',
+						boxShadow: 1,
+					},
+				})}
+			>
+				<Typography variant="button" align="center">
+					{anchorName}
+				</Typography>
+			</Box>
 		</Box>
 	);
 };
