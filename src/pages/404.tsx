261c56a01d0c09e37af02c69b2a25c544d0dc77d
nick-here <zuraniewski.karol@gmail.com>
2022-08-05 21:23:34 +0200

    404 page updated

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import useDocumentTitle from '../hooks/useDocumentTitle';
+import { Box, Button, Typography } from '@mui/material';
 
 const NotFound: NextPage = () => {
 	const { title } = useDocumentTitle('Not found');
@@ -12,7 +13,23 @@ const NotFound: NextPage = () => {
 				<title>{title}</title>
 			</Head>
 
-			<div>404</div>
+			<Box
+				sx={{
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+					flexDirection: 'column',
+					pt: 10,
+				}}
+			>
+				<Typography fontSize={80} color="primary">
+					404
+				</Typography>
+				This page doesn&apos;t seem to exist
+				<Button variant="contained" sx={{ mt: 7 }}>
+					Back to homepage
+				</Button>
+			</Box>
 		</>
 	);
 };
